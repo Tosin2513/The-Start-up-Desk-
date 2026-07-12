496d0509b80ca739ee11ae71df38a227cdec05d6
Tosin2513 <119741838+Tosin2513@users.noreply.github.com>
2026-07-12 18:59:43 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import { Hero } from "@/components/hero"
+import { SiteFooter } from "@/components/site-footer"
 
 export default function Page() {
   return (
-    <main className="min-h-svh bg-background">
+    <main className="min-h-screen bg-background flex flex-col justify-between">
       <Hero />
+      {/* This renders your new compliant footer at the bottom of the home page */}
+      <SiteFooter />
     </main>
   )
 }
