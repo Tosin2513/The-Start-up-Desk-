c97d7264d546781a1da91bbe5d9e6447649ea363
Tosin2513 <119741838+Tosin2513@users.noreply.github.com>
2026-07-12 19:21:06 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,6 @@ export default function Page() {
   return (
     <main className="min-h-screen bg-background flex flex-col justify-between">
       <Hero />
-      {/* This renders your new compliant footer at the bottom of the home page */}
       <SiteFooter />
     </main>
   )
